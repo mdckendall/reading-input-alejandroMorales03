0857917d849f41aec49cd8c0a2df8a20b8b5cab2
alejandroMorales03 <94929338+alejandroMorales03@users.noreply.github.com>
2023-06-27 02:22:41 +0000

    first attempt
    first attempt

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Enter a word:");
-    
-    //Your code will return the length of the entered String
+import java.util.*;
+import java.util.Scanner;
+class Main{
+  public static void main(String[] args){
+    System.out.println("Enter a word: ");
+    Scanner in = new Scanner(System.in);
+    String word;
+    word = in.nextLine();
+    System.out.println(word.length());
   }
 }
